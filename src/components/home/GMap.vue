71463aa0729b6b30726b309b5505c8ad7aa124ee
Bernard Alvero <bernard.alvero@gmail.com>
2019-04-01 09:58:04 +0800

    Added GMap Component and deleted HelloWorld component

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="map">
+    <h2>Map</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GMap",
+  date() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GMap/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import GMap from "@/components/home/GMap.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    GMap
   }
 };
 </script>
