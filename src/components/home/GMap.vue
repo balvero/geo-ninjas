6e2b30ca4cd405e92282cb13c5a117564739c697
Bernard Alvero <bernard.alvero@gmail.com>
2019-04-11 11:27:36 +0800

    Added user location ti user database in firebase

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import firebase from "firebase";
+import db from "@/firebase/init";
 export default {
   name: "GMap",
   data() {
@@ -25,7 +27,44 @@ export default {
     }
   },
   mounted() {
-    this.renderMap();
+    // get current user
+    let user = firebase.auth().currentUser;
+    //get current location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          this.lat = pos.coords.latitude;
+          this.lng = pos.coords.longitude;
+          // find the user record and then update geocoords
+          db.collection("users")
+            .where("user_id", "==", user.uid)
+            .get()
+            .then(snapshot => {
+              snapshot.forEach(doc => {
+                db.collection("users")
+                  .doc(doc.id)
+                  .update({
+                    geolocation: {
+                      lat: pos.coords.latitude,
+                      lng: pos.coords.longitude
+                    }
+                  });
+              });
+            })
+            .then(() => {
+              this.renderMap();
+            });
+        },
+        err => {
+          // timeout, use default values
+          this.renderMap();
+        },
+        { maximumAge: 60000, timeout: 3000 }
+      ); // cached location
+    } else {
+      // position centre by default values
+      this.renderMap();
+    }
   }
 };
 </script>
