61494a8298ee01d1683e11617b94dcfe1db5fa29
Bernard Alvero <bernard.alvero@gmail.com>
2019-04-12 13:45:16 +0800

    Implemented Route Guard

--- src/router.js
+++ src/router.js
@@ -3,17 +3,21 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Signup from "./views/Signup.vue";
 import Login from "./views/Login.vue";
+import firebase from "firebase";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
       name: "Home",
-      component: Home
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/signup",
@@ -27,3 +31,22 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  //check to see if route requires auth
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check auth sate of user
+    let user = firebase.auth().currentUser;
+    if (user) {
+      //user signed in
+      next();
+    } else {
+      // not signed in, redirect to login
+      next({ name: "Login" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
